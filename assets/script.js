7e793b74174573b9ef9fab6a7ef6dae80f378e78
Kelvin Roundtree <kelvinroundtree@live.com>
2023-08-10 18:19:40 -0500

    updated README file

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,5 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-<<<<<<< HEAD
 
-for (var x = 0; x < topics.length; x++) {console.log(topics[x]);
-}
-/*
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
-}
-*/
-=======
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 
@@ -44,4 +27,3 @@ function listTopics() {
 listTopics();
 console.log('Which topic should we study first?');
 selectTopic();
->>>>>>> 025a8c41f6258eea36f99684ae2477a1b2e2b861
